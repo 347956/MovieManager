00c3b6a928767b025602871d6d23cf5aaa969346
Teun Buis <347956@student.fontys.nl>
2021-05-10 13:34:53 +0200

    Added methods to MovieListController

--- MovieManager-TeunBuis/Controllers/MovieListController.cs
+++ MovieManager-TeunBuis/Controllers/MovieListController.cs
@@ -1,4 +1,5 @@
 ﻿using BLL;
+using ContractLayer;
 using Microsoft.AspNetCore.Mvc;
 using MovieManager_TeunBuis.Models;
 using System;
@@ -17,15 +18,36 @@ namespace MovieManager_TeunBuis.Controllers
             List<MovieListModel> movieListModels = new List<MovieListModel>();
             foreach(MovieList movielist in movieListCollection.GetAllMovieListsByUserId(userId))
             {
-                movieListModels.Add(movieListModelFromBO(movielist));
+                movieListModels.Add(MovieListModelFromBO(movielist));
             }
             return View(movieListModels);
         }
         public IActionResult MovieListDetails(int movieListId)
         {
-            MovieListModel movieListModel = movieListModelFromBO(movieListCollection.GetMovieList(movieListId));
+            MovieListModel movieListModel = MovieListModelFromBO(movieListCollection.GetMovieList(movieListId));
             movieListModel.movieModels = GetMoviesForMovieList(movieListModel.movieIds);
             return View(movieListModel);
+        }
+        public IActionResult EditMovieList(int movieListId)
+        {
+            MovieListModel movieListModel = MovieListModelFromBO(movieListCollection.GetMovieList(movieListId));
+            return View(movieListModel);
+        }
+        [HttpPost]
+        public IActionResult EditMovieList(MovieListModel movieListModel)
+        {
+            MovieList movie = new MovieList(CreateMovieListDTOFromViewModel(movieListModel));
+            movie.Update(CreateMovieListDTOFromViewModel(movieListModel));
+            return View();
+        }
+        private MovieListDTO CreateMovieListDTOFromViewModel(MovieListModel movieListModel)
+        {
+            MovieListDTO movieListDTO = new MovieListDTO();
+            movieListDTO.Id = movieListModel.Id;
+            movieListDTO.UserId = movieListModel.UserId;
+            movieListDTO.MovieCount = movieListModel.MovieCount;
+            movieListDTO.Name = movieListModel.Name;
+            return movieListDTO;
         }
         private List<MovieModel> GetMoviesForMovieList(List<int> movieIds)
         {
@@ -48,7 +70,7 @@ namespace MovieManager_TeunBuis.Controllers
             movieModel.ID = movie.ID;
             return movieModel;
         }
-        private MovieListModel movieListModelFromBO(MovieList movieList)
+        private MovieListModel MovieListModelFromBO(MovieList movieList)
         {
             MovieListModel movieListModel = new MovieListModel();
             movieListModel.Id = movieList.Id;
