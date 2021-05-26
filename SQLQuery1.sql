2a1997e62ef91500759f167c7002a34bad0e9853
Teun <347956@student.fontys.nl>
2021-05-26 12:35:39 +0200

    Sync

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -2,4 +2,8 @@
 FROM MovieList_Movies
 INNER JOIN MovieTest ON MovieList_Movies.Movie_Id= MovieTest.Id
 INNER JOIN MovieList ON MovieList_Movies.MovieList_Id = MovieList.Id
+WHERE MovieList.UserId = 0;
+
+SELECT MovieList.Id AS MLID, MovieList.Name
+FROM MovieList
 WHERE MovieList.UserId = 1;
